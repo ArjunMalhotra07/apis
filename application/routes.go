e2293d0f89156bb698934aeeab15645a30f857b8
ArjunMalhotra07 <arjun03.malhotra@gmail.com>
2024-06-15 10:46:49 +0530

    Made order struct and functions

--- application/routes.go
+++ application/routes.go
@@ -3,6 +3,7 @@ package application
 import (
 	"fmt"
 
+	"github.com/ArjunMalhotra07/apis.git/handler"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 )
@@ -12,5 +13,15 @@ func ChiApiRoutes() *chi.Mux {
 	router.Use(middleware.Logger)
 	router.Get("/", BaseRoute)
 	fmt.Println("Running on Port 8080!")
+	router.Route("/orders", loadOrderRoutes)
 	return router
+}
+
+func loadOrderRoutes(router chi.Router) {
+	orderHandler := &handler.Order{}
+	router.Post("/", orderHandler.Create)
+	router.Get("/", orderHandler.List)
+	router.Get("/{id}", orderHandler.GetByID)
+	router.Put("/{id}", orderHandler.UpdateByID)
+	router.Delete("/{id}", orderHandler.DeleteByID)
 }
